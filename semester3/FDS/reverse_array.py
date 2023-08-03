2e1d4346654d5fa0240325e014eef5d8f5b39455
Harsh Banka <harshbanka321@gmail.com>
2023-08-03 08:44:08 +0530

    style: restructure files

---
+++ semester3/FDS/reverse_array.py
@@ -0,0 +1,6 @@
+# print a reversed NumPy array with the element type float
+import numpy
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    return numpy.array(arr[::-1], float)
